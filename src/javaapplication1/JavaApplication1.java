47d46b4b683acbcb682303905429ecda2ec863ec
Kiki <Kiki@Kiki-PC>
2020-10-14 11:34:51 +0700

    Upload Project

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+/**
+ *
+ * @author Kiki
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama Saya : Rizki Dwi nugraha");
+    }
+    
+}
